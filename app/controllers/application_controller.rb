ad4208e8100e3fc4b93fcdbc08c0248304fed296
Malcolm Jeremiah Rajeli <malcolmjeremiahrajeli@gmail.com>
2026-04-24 21:28:23 +0800

    Update application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found(exception)
+    Rails.logger.info("[ApplicationController] #{exception.message}")
+    head :not_found
+  end
 end
