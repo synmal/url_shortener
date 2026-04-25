53944fb577444c2b6be5f61d07f689cd24d495b7
Malcolm Jeremiah Rajeli <malcolmjeremiahrajeli@gmail.com>
2026-04-25 10:01:34 +0800

    Add begin rescue

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -2,8 +2,12 @@ class ShortUrlsController < ApplicationController
   def show
     short_url = ShortUrl.find_by!(slug: params[:slug])
 
-    short_url.visits.create!(ip_address: request.remote_ip, visited_at: Time.current)
-    short_url.increment_visits!
+    begin
+      short_url.visits.create!(ip_address: request.remote_ip, visited_at: Time.current)
+      short_url.increment_visits!
+    rescue ActiveRecord::RecordInvalid, ActiveRecord::StatementInvalid => e
+      Rails.logger.warn("[ShortUrlsController] Visit tracking failed: #{e.message}")
+    end
 
     redirect_to short_url.target_url.url, allow_other_host: true, status: :found
   end
