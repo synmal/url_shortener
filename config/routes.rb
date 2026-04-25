56c644839d40e9572167d9d49fdc3f7de06c24f0
Malcolm Jeremiah Rajeli <malcolmjeremiahrajeli@gmail.com>
2026-04-25 23:06:39 +0800

    Move Sidekiq web mount above slug catch-all route
    The /:slug constraint matches "sidekiq", intercepting the request
    before it reaches the Sidekiq::Web mount. Also set default
    Sidekiq web password to "password".

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  require "sidekiq/web"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(
+      username, ENV.fetch("SIDEKIQ_WEB_USERNAME", "admin")
+    ) & ActiveSupport::SecurityUtils.secure_compare(
+      password, ENV.fetch("SIDEKIQ_WEB_PASSWORD", "password")
+    )
+  end
+
+  mount Sidekiq::Web => "/sidekiq"
+
   devise_for :users
   root "target_urls#new"
 
@@ -10,14 +21,4 @@ Rails.application.routes.draw do
   get "/:slug/metrics", to: "metrics#show", as: :short_url_metrics, constraints: { slug: /[1-9A-HJ-NP-Za-km-z]{4,15}/ }
 
   get "up" => "rails/health#show", as: :rails_health_check
-
-  require "sidekiq/web"
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    ActiveSupport::SecurityUtils.secure_compare(
-      username, ENV.fetch("SIDEKIQ_WEB_USERNAME", "admin")
-    ) & ActiveSupport::SecurityUtils.secure_compare(
-      password, ENV.fetch("SIDEKIQ_WEB_PASSWORD", SecureRandom.hex)
-    )
-  end
-  mount Sidekiq::Web => "/sidekiq"
 end
